b03cef8e39224ce3365492308db11f4adc2c9dc1
zloy05786-ux <zloy05786@gmail.com>
2026-01-15 21:20:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,13 +16,6 @@ function update() {
   document.getElementById("value").textContent = val;
 }
 
-function tap(btn) {
-  btn.animate(
-    [{ transform: "scale(1)" }, { transform: "scale(.9)" }, { transform: "scale(1)" }],
-    { duration: 120 }
-  );
-}
-
 function check() {
   document.getElementById("error").classList.remove("hidden");
 }
